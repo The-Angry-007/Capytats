488f646d5abacaeac9e2c1d14edcd8bf74dd3ad7
kieran <46075444+The-Angry-007@users.noreply.github.com>
2025-04-03 23:08:45 +0100

    added game objects for hotbar and inventory

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -5,11 +5,11 @@ public class Inventory : MonoBehaviour
     Item[,] items;
     public Vector2Int invSize;
     public GameObject invGUI;
+    public GameObject hotbarGUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         items = new Item[invSize.x,invSize.y];
-        invGUI.GetComponent<RawImage>().texture = ItemManager.itemTextures[0];
     }
 
     // Update is called once per frame
