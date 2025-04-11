3de4fe11c3596437b637c444ca43f6dec45098c2
Becca4797 <rebeccaau07@gmail.com>
2025-04-11 22:07:17 +0100

    Destroy tree attempt
    FAILED ATTEMPT

---
+++ Assets/TreeObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Check for mouse click 
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform == transform)
+                {
+                    Debug.Log("Hello");
+                    Destroy(gameObject); 
+                }
+            }
+        }   
+    }
+}
